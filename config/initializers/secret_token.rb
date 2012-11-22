6f40edcef22bbdaf959d7deed8832e30d811d362
= <=>
2012-11-22 12:24:15 +0800

    microblog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThirdApp::Application.config.secret_token = '238673ae8e2349a70ffc9788152931d83b93b8fd3027d2283d19511b495b0a5469174bfb914dc89f203fe72317993b36d7ac985f783198381ddf485db7624bba'
